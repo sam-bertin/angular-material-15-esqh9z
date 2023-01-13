9dd601590d3f710588779d9b377199fe4e5a5687
sam-bertin <116896072+sam-bertin@users.noreply.github.com>
2023-01-13 17:12:12 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,39 @@
-import { Component,} from '@angular/core';
+import { Component } from '@angular/core';
+
+type Card = {
+  title: string;
+  subtitle: string;
+  imgSource: string;
+  cardContent: string;
+  action1: string;
+  action2: string;
+  cardFooter: string;
+};
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
-type Card =  {
-  title : string;
-  subtitle : string;
-  imgSource: string;
-  cardContent: string;
-  action1: string;
-  action2: string;
-  cardFooter: string;
-}
 export class AppComponent {
-  liste: Card[] = [{title:"Titre 1",
-                    subtitle: "Sous titre 1",
-                    imgSource: "link",
-                    cardContent:"Content 1",
-                    action1:"Action 1",
-                    action2:"Action 2",
-                    cardFooter: "Footer"}];
+  liste: Card[] = [
+    {
+      title: 'Titre 1',
+      subtitle: 'Sous titre 1',
+      imgSource: 'link',
+      cardContent: 'Content 1',
+      action1: 'Action 1',
+      action2: 'Action 2',
+      cardFooter: 'Footer',
+    },
+    {
+      title: 'Titre 2',
+      subtitle: 'Sous titre 2',
+      imgSource: 'link',
+      cardContent: 'Content 2',
+      action1: 'Action 2',
+      action2: 'Action 3',
+      cardFooter: 'Footer',
+    },
+  ];
 }
